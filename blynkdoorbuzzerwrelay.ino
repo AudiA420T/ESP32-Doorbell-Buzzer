bad00e15159c27a4b02f72d3bb95cdea49396f09
Clint Masterson <Audia4_18t@yahoo.com>
2024-07-12 21:24:22 -0700

    Cleaned up code, added WiFi check
    Noticed a few rare times where the unit went offline. Put in a WiFi check. Also cleaned up the timers to align with Blynk

--- blynkdoorbuzzerwrelay.ino
+++ blynkdoorbuzzerwrelay.ino
@@ -21,7 +21,9 @@
 char ssid[] = "YourNetworkName";
 char pass[] = "YourNetworkPassword";
 
-BlynkTimer timer;
+BlynkTimer uptimeTimer;
+BlynkTimer buzzerCheckTimer;
+BlynkTimer wifiCheckTimer;
 
 const int inputPin = 12;
 int acValue = digitalRead(inputPin);
@@ -40,7 +42,7 @@ BLYNK_WRITE(V6)
   Blynk.virtualWrite(V6, 1);
   delay(5000);
   Blynk.virtualWrite(V6, 0);
-  digitalWrite(2,LOW);  // Set digital pin 2 HIGH
+  digitalWrite(2,LOW);  // Set digital pin 2 LOW
 }
 
 // This function is called every time somone triggers the relay remotely
@@ -50,13 +52,13 @@ BLYNK_WRITE(V7)
   digitalWrite(2,HIGH);  // Set digital pin 2 HIGH
   delay(relaycontroltime);
   digitalWrite(13, LOW);
-  digitalWrite(2,LOW);  // Set digital pin 2 HIGH
+  digitalWrite(2,LOW);  // Set digital pin 2 LOW
   delay(250);
   digitalWrite(13, HIGH);
   digitalWrite(2,HIGH);  // Set digital pin 2 HIGH
   delay(relaycontroltime);
   digitalWrite(13, LOW);
-  digitalWrite(2,LOW);  // Set digital pin 2 HIGH
+  digitalWrite(2,LOW);  // Set digital pin 2 LOW
 }
 
 // This function is called every time the device is connected to the Blynk.Cloud
@@ -66,44 +68,18 @@ BLYNK_CONNECTED()
 }
 
 // This function sends Arduino's uptime every second to Virtual Pin 2.
-void myTimerEvent()
+void uptimeEvent()
 {
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
   Blynk.virtualWrite(V2, millis() / 1000);
 }
 
-void setup()
+// This function checks for someone buzzing and sends a push notification
+void buzzerCheckEvent()
 {
-  // Debug console
-  Serial.begin(115200);
-
-  pinMode(2, OUTPUT); // Initialise digital pin 2 as an output pin
-
-  //setting up the relay
-  pinMode(13,OUTPUT);
-
-  //setting up the optocoupler
-  pinMode(12, INPUT_PULLUP);
-
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
-  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
-
-  // Setup a function to be called every second
-  timer.setInterval(1000L, myTimerEvent);
-}
-
-void loop()
-{
-  Blynk.run();
-  timer.run();
-  
   // Reading optocoupler value
   acValue = digitalRead(12);
-  //Serial.println(acValue) - Sending this to the server for now to not bog the system down
-  delay(100);
 
   if(acValue == 0) {
     Serial.println("got a buzz, sending push");
@@ -118,3 +94,44 @@ void loop()
   }
 }
 
+// This function checks if WiFi is connected, otherwise resets the ESP32
+void wifiCheckEvent()
+{
+  // Checking WiFi status. If not connected, reset ESP32
+  if(WiFi.status() != WL_CONNECTED) {
+     ESP.restart();
+  }
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  // Initialise digital pin 2 as an output pin
+  pinMode(2, OUTPUT);
+  //setting up the relay
+  pinMode(13,OUTPUT);
+  //setting up the optocoupler
+  pinMode(12, INPUT_PULLUP);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // First timer gets called every second for heartbeat
+  uptimeTimer.setInterval(1000L, uptimeEvent);
+  // Second timer gets called 10 times per second for incoming buzzes
+  buzzerCheckTimer.setInterval(100L, buzzerCheckEvent);
+  // First timer gets called every 10 minutes for WiFi check
+  wifiCheckTimer.setInterval(600000L, wifiCheckEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  uptimeTimer.run();
+  buzzerCheckTimer.run();
+  wifiCheckTimer.run();
+}
